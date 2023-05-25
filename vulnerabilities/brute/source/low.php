71015468cfcbea03b63dca887bcdfd423b71faac
Angelo Benavidez <abenavidez.dev@gmail.com>
2023-05-25 18:50:30 -0500

    Correccion de vulnerabilidad 2

--- vulnerabilities/brute/source/low.php
+++ vulnerabilities/brute/source/low.php
@@ -9,7 +9,10 @@ if( isset( $_GET[ 'Login' ] ) ) {
 	$pass = md5( $pass );
 
 	// Check the database
-	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
+	//Forma original
+	//$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
+	//Forma corregida evitando sql inJection
+	$query  =Users::where(user ='$user')->where(password ='$pass');
 	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
