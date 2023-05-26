b5b159e265e8eccd2992e904a16afef7661346e3
Angelo Benavidez <abenavidez.dev@gmail.com>
2023-05-25 19:15:28 -0500

    Correccion de vulnerabilidad 3

--- vulnerabilities/brute/source/low.php
+++ vulnerabilities/brute/source/low.php
@@ -12,7 +12,9 @@ if( isset( $_GET[ 'Login' ] ) ) {
 	//Forma original
 	//$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
 	//Forma corregida evitando sql inJection
-	$query  =Users::where(user ='$user')->where(password ='$pass');
+	$query  =Users::where(user ='$user')
+			->where(password ='$pass')
+			-> first();//Se utiliza para obtener el primer registro que cumple con las condiciones
 	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
